dffdf5afac221b9003f19e09d86467a7c902071e
brusinow <brusinow@me.com>
2016-06-14 17:32:19 -0700

    added auth but have it currently commented out

--- RoadWarrior/www/js/controllers.js
+++ RoadWarrior/www/js/controllers.js
@@ -8,7 +8,38 @@ $scope.event = {
   city: "Seattle, WA",
 }
 
+// $scope.login = function() {
+//     Auth.$authWithOAuthRedirect("facebook").then(function(authData) {
+//       // User successfully logged in
+//     }).catch(function(error) {
+//       if (error.code === "TRANSPORT_UNAVAILABLE") {
+//         Auth.$authWithOAuthPopup("facebook").then(function(authData) {
+//           // User successfully logged in. We can log to the console
+//           // since we’re using a popup here
+//           console.log(authData);
+//         });
+//       } else {
+//         // Another error occurred
+//         console.log(error);
+//       }
+//     });
+// };
+
+// Auth.$onAuth(function(authData) {
+//   if (authData === null) {
+//     console.log("Not logged in yet");
+//   } else {
+//     console.log("Logged in as", authData.uid);
+//   }
+//   $scope.authData = authData; // This will display the user's name in our view
+// });
+
+
+
+
 }])
+
+
 
 
 .controller('ChatsCtrl', function($scope, Chats) {
